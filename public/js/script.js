df3ccad6115464e8e6b513f8124b8866df34c5fe
Andrea Pallotta <ap4534@rit.edu>
2020-11-27 21:03:39 -0500

    created new pages. started working on main container

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0)
   
     if ($navbarBurgers.length > 0) {
 
         $navbarBurgers.forEach( el => {
 
+          el.addEventListener('click', () => {
+            const target = el.dataset.target;
+            const $target = document.getElementById(target);
+            el.classList.toggle('is-active');
+            $target.classList.toggle('is-active')
+          })
+
+      })
+    }
+
+    const $menuItem = Array.prototype.slice.call(document.querySelectorAll('.menu-item'), 0)
+
+    if ($navbarBurgers.length > 0) {
+
+      $navbarBurgers.forEach( el => {
+
         el.addEventListener('click', () => {
           const target = el.dataset.target;
           const $target = document.getElementById(target);
           el.classList.toggle('is-active');
-          $target.classList.toggle('is-active');
-        });
+          $target.classList.toggle('is-active')
+        })
 
-      });
-      
+      })
     }
+
+  })
+
   
-  });
